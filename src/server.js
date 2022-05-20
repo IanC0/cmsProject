bcfd2feb73aa6964be257d1c13e078a9d4217338
Michael Mullany <mamullany@gmail.com>
2022-05-20 15:34:06 +0100

    Add server.js file

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require("./db/connection");
+const express = require("express");
+const cors = require("cors");
+
+const userRouter = require("./user/userRoutes");
+const groupRouter = require("./group/groupRoutes");
+const componentRouter = require("./component/componentRoutes");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(userRouter);
+app.use(groupRouter);
+app.use(componentRouter);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
