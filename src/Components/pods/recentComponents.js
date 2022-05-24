9901f074ae6071454662206d2fce104e77b3cf10
Ian Connell <ian10gt@gmail.com>
2022-05-24 12:38:01 +0100

    table added, no functionality yet

--- src/Components/pods/recentComponents.js
+++ src/Components/pods/recentComponents.js
@@ -7,7 +7,25 @@ export const RecentComponents = () => {
             </div>
             <div id="recentComponentsBodyContainer">
                 <div id="recentComponentsBody">
-            
+                    <div className="podTableContainer">
+                        <table className="podTable">
+                            <tr>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Modified</th>
+                            </tr>
+                            <tr>
+                                <td>Example Name</td>
+                                <td>Text box</td>
+                                <td>01/01/2022</td>
+                            </tr>
+                            <tr>
+                                <td>Example Name 2</td>
+                                <td>Button</td>
+                                <td>01/02/2022</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
             
