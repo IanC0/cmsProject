8f4731c576229def5ad4cd1b1964bddcafde753c
Michael Mullany <mamullany@gmail.com>
2022-05-26 17:30:16 +0100

    Manage Groups and table styles

--- src/Components/mainApp.js
+++ src/Components/mainApp.js
@@ -7,16 +7,18 @@ import { BannerPod } from './pods/BannerPod'
 import { MediaPod } from './pods/MediaPod'
 import { ButtonPod } from "./pods/ButtonPod";
 import { NewFormPod } from './pods/NewFormPod'
+import { ManageGroupsPod } from "./pods/ManageGroupsPod";
 
 export const MainApp = () => {
 
     const [appState, setAppState] = useState();
+    const [appMode, setAppMode] = useState();
     const [addingGroup, setAddingGroup] = useState();
     const [groupName, setGroupName] = useState();
 
     return (
         <div id="mainContainer">
-            <Sidebar />
+            <Sidebar setAppState={setAppState} />
             <main id="mainBodyContainer">
                 {
                     (!appState || appState == "Welcome") &&
@@ -30,6 +32,10 @@ export const MainApp = () => {
                             setGroupName={setGroupName}
                         />
                     </>
+                }
+                {
+                    appState=="ManageGroups" &&
+                    <ManageGroupsPod setAppState={setAppState} />
                 }
                 {
                     appState=="AddTextbox" &&
