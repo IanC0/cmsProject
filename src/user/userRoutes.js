aaeaed015475fb39e3b66d295f284c1bc241f50b
Ian Connell <ian10gt@gmail.com>
2022-05-26 14:51:13 +0100

    correction

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,12 +1,10 @@
 const { Router } = require("express");
 const { addUser, login, listUsers } = require("./userControllers");
 const { hashPass, unHash } = require("../middleware");
-import { addComponent } = require("../componentCommands/comonentControllers")
 
 const userRouter = Router();
 
 userRouter.post("/login", unHash, login);
 userRouter.post("/user", hashPass, addUser);
-userRouter.post("/component", addComponent)
 
 module.exports = userRouter;
